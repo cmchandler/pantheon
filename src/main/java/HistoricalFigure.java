80902f30ceb9c7469d6144054a44774db259fc14
Christien Chandler <christienchandler@gmail.com>
2017-04-18 16:50:26 -0700

    Started with implementation of utility classes.

---
+++ src/main/java/HistoricalFigure.java
@@ -0,0 +1,9 @@
+package main.java;
+
+/**
+ * HistoricalFigure: represents a historical figure datapoint from the Pantheon data set
+ */
+public class HistoricalFigure {
+
+
+}
---
+++ src/main/java/HistoricalFigureBuilder.java
@@ -0,0 +1,99 @@
+package main.java;
+
+/**
+ * Created by chris on 4/17/17.
+ */
+public class HistoricalFigureBuilder {
+
+    private int _article_id;
+    private String _full_name;
+    private String _sex;
+    private int _birth_year;
+    private String _city;
+    private String _state;
+    private String _country;
+    private String _continent;
+    private double _latitude;
+    private double _longitude;
+    private String _occupation;
+    private String _industry;
+    private String _domain;
+    private int _article_languages;
+    private int _page_views;
+    private int _average_views;
+    private double _historical_popularity_index;
+
+    public HistoricalFigureBuilder x(String _x) {
+        this._x = _x;
+        return this;
+    }
+
+    public HistoricalFigureBuilder article_id(int _article_id ) {
+        this._article_id = _article_id;
+        return this;
+    }
+
+    public HistoricalFigureBuilder full_name(String _full_name) {
+        this._full_name = _full_name;
+        return this;
+    }
+
+    public HistoricalFigureBuilder sex(String _sex) {
+        this._sex= _sex;
+        return this;
+    }
+
+    public HistoricalFigureBuilder birth_year(int _birth_year) {
+        this._birth_year = _birth_year;
+        return this;
+    }
+
+    public HistoricalFigureBuilder _city(String _city) {
+        this._city = _city;
+        return this;
+    }
+
+    public HistoricalFigureBuilder state(String _state) {
+        this._state = _state;
+        return this;
+    }
+
+    public HistoricalFigureBuilder country(String _country) {
+        this._country = _country;
+        return this;
+    }
+
+    public HistoricalFigureBuilder continent(String _continent) {
+        this._continent = _continent;
+        return this;
+    }
+
+    public HistoricalFigureBuilder latitude(double _latitude) {
+        this._latitude = _latitude;
+        return this;
+    }
+
+    public HistoricalFigureBuilder longitude(double _longitude) {
+        this._longitude = _longitude;
+        return this;
+    }
+
+    public HistoricalFigureBuilder x(String _x) {
+        this._x = _x;
+        return this;
+    }
+
+    public HistoricalFigureBuilder x(String _x) {
+        this._x = _x;
+        return this;
+    }
+
+    public HistoricalFigureBuilder x(String _x) {
+        this._x = _x;
+        return this;
+    }
+
+
+
+
+}
