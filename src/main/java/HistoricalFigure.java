60efd478ad5cf38dbd29f5ac8a233319c615b9e1
Christien Chandler <chandlcm@plu.edu>
2017-04-18 18:04:08 -0700

    Made HistoricalFigureBuilder a nested class of HistoricalFigure.  Made constructor for PantheonDataSet that hopefully loads the data into an arraylist

--- src/main/java/HistoricalFigure.java
+++ src/main/java/HistoricalFigure.java
@@ -24,177 +24,207 @@ public class HistoricalFigure {
     private int average_views;
     private double historical_popularity_index;
 
-    public HistoricalFigure (
-             int article_id,
-             String full_name,
-             String sex,
-             int birth_year,
-             String city,
-             String state,
-             String country,
-             String continent,
-             double latitude,
-             double longitude,
-             String occupation,
-             String industry,
-             String domain,
-             int article_languages,
-             int page_views,
-             int average_views,
-             double historical_popularity_index )
+    private HistoricalFigure ( HistoricalFigureBuilder builder)
     {
-          this.article_id = article_id;
-          this.full_name = full_name;
-          this.sex = sex;
-          this.birth_year = birth_year;
-          this.city = city;
-          this.state = state;
-          this.country = country;
-          this.continent = continent;
-          this.latitude = latitude;
-          this.longitude = longitude;
-          this.occupation = occupation;
-          this.industry = industry;
-          this.domain = domain;
-          this.article_languages = article_languages;
-          this.page_views = page_views;
-          this.average_views = average_views;
-          this.historical_popularity_index = historical_popularity_index;
+          this.article_id = builder._article_id;
+          this.full_name = builder._full_name;
+          this.sex = builder._sex;
+          this.birth_year = builder._birth_year;
+          this.city = builder._city;
+          this.state = builder._state;
+          this.country = builder._country;
+          this.continent = builder._continent;
+          this.latitude = builder._latitude;
+          this.longitude = builder._longitude;
+          this.occupation = builder._occupation;
+          this.industry = builder._industry;
+          this.domain = builder._domain;
+          this.article_languages = builder._article_languages;
+          this.page_views = builder._page_views;
+          this.average_views = builder._average_views;
+          this.historical_popularity_index = builder._historical_popularity_index;
     }
 
     public int getArticle_id() {
         return article_id;
     }
 
-    public void setArticle_id(int article_id) {
-        this.article_id = article_id;
-    }
-
     public String getFull_name() {
         return full_name;
-    }
-
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
     }
 
     public String getSex() {
         return sex;
     }
 
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
     public int getBirth_year() {
         return birth_year;
-    }
-
-    public void setBirth_year(int birth_year) {
-        this.birth_year = birth_year;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getState() {
         return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 
     public String getCountry() {
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
 
     public String getContinent() {
         return continent;
-    }
-
-    public void setContinent(String continent) {
-        this.continent = continent;
     }
 
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public double getLongitude() {
         return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 
     public String getOccupation() {
         return occupation;
     }
 
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
-
     public String getIndustry() {
         return industry;
-    }
-
-    public void setIndustry(String industry) {
-        this.industry = industry;
     }
 
     public String getDomain() {
         return domain;
     }
 
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
     public int getArticle_languages() {
         return article_languages;
-    }
-
-    public void setArticle_languages(int article_languages) {
-        this.article_languages = article_languages;
     }
 
     public int getPage_views() {
         return page_views;
     }
 
-    public void setPage_views(int page_views) {
-        this.page_views = page_views;
-    }
-
     public int getAverage_views() {
         return average_views;
-    }
-
-    public void setAverage_views(int average_views) {
-        this.average_views = average_views;
     }
 
     public double getHistorical_popularity_index() {
         return historical_popularity_index;
     }
 
-    public void setHistorical_popularity_index(double historical_popularity_index) {
-        this.historical_popularity_index = historical_popularity_index;
+    /**
+     * Constructs a HistoricalFigure
+     */
+    public static class HistoricalFigureBuilder {
+
+        private int _article_id;
+        private String _full_name;
+        private String _sex;
+        private int _birth_year;
+        private String _city;
+        private String _state;
+        private String _country;
+        private String _continent;
+        private double _latitude;
+        private double _longitude;
+        private String _occupation;
+        private String _industry;
+        private String _domain;
+        private int _article_languages;
+        private int _page_views;
+        private int _average_views;
+        private double _historical_popularity_index;
+
+        public HistoricalFigureBuilder article_id(int _article_id ) {
+            this._article_id = _article_id;
+            return this;
+        }
+
+        public HistoricalFigureBuilder full_name(String _full_name) {
+            this._full_name = _full_name;
+            return this;
+        }
+
+        public HistoricalFigureBuilder sex(String _sex) {
+            this._sex= _sex;
+            return this;
+        }
+
+        public HistoricalFigureBuilder birth_year(int _birth_year) {
+            this._birth_year = _birth_year;
+            return this;
+        }
+
+        public HistoricalFigureBuilder city(String _city) {
+            this._city = _city;
+            return this;
+        }
+
+        public HistoricalFigureBuilder state(String _state) {
+            this._state = _state;
+            return this;
+        }
+
+        public HistoricalFigureBuilder country(String _country) {
+            this._country = _country;
+            return this;
+        }
+
+        public HistoricalFigureBuilder continent(String _continent) {
+            this._continent = _continent;
+            return this;
+        }
+
+        public HistoricalFigureBuilder latitude(double _latitude) {
+            this._latitude = _latitude;
+            return this;
+        }
+
+        public HistoricalFigureBuilder longitude(double _longitude) {
+            this._longitude = _longitude;
+            return this;
+        }
+
+        public HistoricalFigureBuilder occupation(String _occupation) {
+            this._occupation = _occupation;
+            return this;
+        }
+
+        public HistoricalFigureBuilder industry(String _industry) {
+            this._industry = _industry;
+            return this;
+        }
+
+        public HistoricalFigureBuilder domain(String _domain) {
+            this._domain = _domain;
+            return this;
+        }
+
+        public HistoricalFigureBuilder article_languages(int _article_languages) {
+            this._article_languages = _article_languages;
+            return this;
+        }
+
+        public HistoricalFigureBuilder page_views(int _page_views) {
+            this._page_views = _page_views;
+            return this;
+        }
+
+        public HistoricalFigureBuilder average_views(int _average_views) {
+            this._average_views = _average_views;
+            return this;
+        }
+
+        public HistoricalFigureBuilder historical_popularity_index(double _historical_popularity_index) {
+            this._historical_popularity_index = _historical_popularity_index;
+            return this;
+        }
+
+        public HistoricalFigure build() {
+           HistoricalFigure figure = new HistoricalFigure(this);
+           return figure;
+        }
     }
 }
--- src/main/java/PantheonDataSet.java
+++ src/main/java/PantheonDataSet.java
@@ -1,7 +1,63 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
- * Created by chris on 4/18/2017.
+ * PantheonDataSet: holds the main data
  */
 public class PantheonDataSet {
+
+    private ArrayList<HistoricalFigure> dataset;
+
+    public PantheonDataSet(String filename) {
+
+        dataset = new ArrayList<>();
+        BufferedReader br = null;
+        String line = "";
+
+        try {
+            br = new BufferedReader(new FileReader(filename));
+            while ((line = br.readLine()) != null) {
+                String[] readData = line.split(",");
+                HistoricalFigure h = new HistoricalFigure.HistoricalFigureBuilder()
+                        .article_id(Integer.parseInt(readData[0]))
+                        .full_name(readData[1])
+                        .sex(readData[2])
+                        .birth_year(Integer.parseInt(readData[3]))
+                        .city(readData[4])
+                        .state(readData[5])
+                        .country(readData[6])
+                        .continent(readData[7])
+                        .latitude(Double.parseDouble(readData[8]))
+                        .longitude(Double.parseDouble(readData[9]))
+                        .occupation(readData[10])
+                        .industry(readData[11])
+                        .domain(readData[12])
+                        .article_languages(Integer.parseInt(readData[13]))
+                        .page_views(Integer.parseInt(readData[14]))
+                        .average_views(Integer.parseInt(readData[15]))
+                        .historical_popularity_index(Double.parseDouble(readData[16]))
+                        .build();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("IO Exception." );
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println("IO Exception: EOF");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
